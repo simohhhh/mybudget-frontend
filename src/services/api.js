45f989f0193ae797a200da5706bde96819923d60
simohhhh <aittalbm8@gmail.com>
2026-05-14 18:05:26 +0100

    fix: connexion au nouveau backend Vercel

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // configuration d'axios
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api', 
+  baseURL: 'https://mybudget-backend-seven.vercel.app/api', 
 });
 
 // intercepteur pour attacher le token JWT aux requetes
